88e93e9c884ade54c355a0891a9b7062e3e36265
Ben Underwood <benu423@gmail.com>
2025-04-21 14:27:14 -0400

    Added working code from lab computer

--- StepLength.h
+++ StepLength.h
@@ -1,6 +1,8 @@
 #ifndef _STEP_LENGTH_H_
 #define _STEP_LENGTH_H_
 
+//#include "Arduino_BMI270_BMM150.h"
+#include "Adafruit_DRV2605.h"
 #include "Ultrasonic.h"
 
 #define BURST_LENGTH                10
@@ -9,13 +11,14 @@
 #define CALIBRATION_STEPS           5
 #define POLL_DELAY_MS               10
 #define ALGO_WINDOW_LEN              4
-const float PCT_THRESH = 0.2;
+const float PCT_THRESH = 0.8;
 #define NO_STEP_MAX_TIME          5000
 
 extern float target_step_length;
 extern float burst[];
 
-void runCalibration(Ultrasonic device);
-float getStepLength(Ultrasonic device);
+void runCalibration(Ultrasonic *device);
+float getStepLength(Ultrasonic *device);
+void runAlgorithm(Ultrasonic *device, Adafruit_DRV2605 *buzzer);
 
 #endif
